370fad6b5bd6b6d88b952eb24feaf477c1077534
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2020-10-11 21:53:23 -0400

    start a ui

--- src/Views/Example.java
+++ src/Views/Example.java
@@ -1,3 +1,5 @@
+package Views;
+
 import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
---
+++ src/Views/OptionsLayoutView.java
@@ -0,0 +1,48 @@
+package Views;
+
+import javax.swing.*;
+import java.awt.*;
+
+class Pain2 extends JFrame {
+    MyPanel panel;
+
+    // constructor
+    Pain2() {
+
+        panel = new MyPanel();
+        this.add(panel);
+        //TODO: Close window don't exit app lol
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        // add the label to the JFrame
+    }
+
+}
+
+class MyPanel extends JPanel {
+
+    public MyPanel() {
+        JButton jrb1 = new JButton("Button1");
+        JButton jrb2 = new JButton("Button2");
+        JButton jrb3 = new JButton("Button3");
+        Box box1 = Box.createVerticalBox();
+        box1.add(jrb1);
+        box1.add(jrb2);
+        box1.add(jrb3);
+
+
+        setLayout(new GridLayout(1, 1));
+        add(box1);
+
+
+    }
+
+}
+//public class TestFrame2 {
+//    public static void main(String[] args) {
+//        Pain1 frame = new Pain1("Hello"); // construct a MyFrame object
+//        frame.setVisible(true);             // ask it to become visible
+//    }
+//}
